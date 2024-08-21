5d39ad4153f428b27633292fff8e58154350741a
m0hn-z <mohn08052006@gmail.com>
2024-08-21 14:58:32 +0530

    Added the Jewelry Section

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 
     const firstItem = document.querySelector('#first-item');
     const secontItem = document.querySelector('#second-item')
+    const thirdItem = document.querySelector('#third-item')
+    const fourthItem = document.querySelector('#fourth-item')
     const newInElement = document.querySelector('.new-in');
     const designers = document.querySelector('.designers');
+    const women = document.querySelector('.women');
+    const jewelry = document.querySelector('.jewelry');
 
     firstItem.addEventListener('mouseover', () => {
         newInElement.style.display = 'flex';
@@ -33,4 +37,34 @@
     designers.addEventListener('mouseout', () => {
         designers.style.display = 'none';
     });
+    
+
+    // thirdItem.addEventListener('mouseover', () => {
+    //     women.style.display = 'block';
+    // });
+
+    // thirdItem.addEventListener('mouseout', () => {
+    //     women.style.display = 'none';   
+    // });
+    // designers.addEventListener('mouseover', () => {
+    //     women.style.display = 'block';
+    // });
+    // designers.addEventListener('mouseout', () => {
+    //     women.style.display = 'none';
+    // });
+
+    fourthItem.addEventListener('mouseover', () => {
+        jewelry.style.display = 'flex';
+    });
+
+    fourthItem.addEventListener('mouseout', () => {
+        jewelry.style.display = 'none';   
+    });
+    jewelry.addEventListener('mouseover', () => {
+        jewelry.style.display = 'flex';
+    });
+    jewelry.addEventListener('mouseout', () => {
+        jewelry.style.display = 'none';
+    });
+
 
